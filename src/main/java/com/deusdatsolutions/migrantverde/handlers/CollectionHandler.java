6a15d25dcc699d88da0d09312d3aa5c0b8ed7ccc
J Patrick Davenport <patrick.davenport@deusdatsolutions.com>
2016-07-11 00:01:57 -0400

    Adds support for more complex collection creations

--- src/main/java/com/deusdatsolutions/migrantverde/handlers/CollectionHandler.java
+++ src/main/java/com/deusdatsolutions/migrantverde/handlers/CollectionHandler.java
@@ -2,18 +2,34 @@ package com.deusdatsolutions.migrantverde.handlers;
 
 import com.arangodb.ArangoDriver;
 import com.arangodb.ArangoException;
+import com.arangodb.entity.CollectionOptions;
 import com.deusdatsolutions.migrantverde.jaxb.CollectionOperationType;
 
 public class CollectionHandler implements IMigrationHandler<CollectionOperationType> {
 
 	@Override
 	public void migrate(final CollectionOperationType migration, final ArangoDriver driver) throws ArangoException {
+		final String name = migration.getName();
 		switch (migration.getAction()) {
 		case CREATE:
-			driver.createCollection(migration.getName());
+			final CollectionOptions co = new CollectionOptions();
+			co.setDoCompact(migration.isCompactable());
+			co.setIsVolatile(migration.isVolatile());
+			if (migration.getJournalSize() != null) {
+				co.setJournalSize(migration.getJournalSize());
+			}
+			if (migration.getNumberOfShards() != null) {
+				co.setNumberOfShards(migration.getNumberOfShards());
+			}
+			co.setWaitForSync(migration.isWaitForSync());
+
+			if (!migration.getShardKey().isEmpty()) {
+				co.setShardKeys(migration.getShardKey());
+			}
+			driver.createCollection(name, co);
 			break;
 		case DROP:
-			driver.deleteCollection(migration.getName());
+			driver.deleteCollection(name);
 			break;
 		default:
 			throw new IllegalArgumentException("Can't hand migration " + migration.getAction());
